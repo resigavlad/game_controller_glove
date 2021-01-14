c273739473f0dbffef77da659eea240cb4e7d2f0
Rhobar2000 <57294238+Rhobar2000@users.noreply.github.com>
2021-01-14 17:02:18 +0200

    fixed input lag

--- Receiver_code.ino
+++ Receiver_code.ino
@@ -5,6 +5,7 @@
 SoftwareSerial configBt(rx, tx);
 
 int resultVector[] = {0, 5, 5};
+int resultVectorCopy[] = {0, 5, 5}; //used for testing if there is a change in received data
 int resultVectorLength = sizeof(resultVector) / sizeof(int) - 1;
 
 void setup() {
@@ -16,6 +17,16 @@ void setup() {
   Serial.begin(115200);
 }
 
+int detectChange() {
+  for(int i = 0; i < resultVectorLength; i++) {
+    if(resultVector[1] != resultVectorCopy[1]) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
 void loop() {
   if(configBt.available() > 0)
   {
@@ -24,13 +35,18 @@ void loop() {
     resultVector[aux / 10 % 10] = aux % 10 + aux / 100 * 10; 
   }
 
-  //send data to pc via usb in format %number,%number,%number\n
-  for(int i = 0; i < resultVectorLength; i++) {
-    Serial.print(resultVector[i]);
-    Serial.flush();
-    Serial.print(",");
+  if(detectChange() == 1) {
+
+    //send data to pc via usb in format %number,%number,%number\n
+    for(int i = 0; i < resultVectorLength; i++) {
+      Serial.print(resultVector[i]);
+      Serial.flush();
+      Serial.print(",");
+      Serial.flush();
+
+      resultVectorCopy[1] = resultVector[1];
+    }
+    Serial.println(resultVector[resultVectorLength]);
     Serial.flush();
   }
-  Serial.println(resultVector[resultVectorLength]);
-  Serial.flush();
 }
